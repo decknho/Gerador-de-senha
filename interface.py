6e36efe578592bf3b45acc3a8895c8b347a9376e
Dereck Eder <decknho26@gmail.com>
2023-01-23 11:22:18 -0300

    função senha finalizada

---
+++ interface.py
@@ -0,0 +1,15 @@
+def senhas():
+    from caracteres import caractere
+    from random import randint
+    quantidade = int(input('Quantas senhas você quer? '))
+    tamanho = int(input('Quantos caracteres você quer para sua senhas? '))
+    print()
+    cont = 1
+    print('-=' * 5, 'Senhas geradas', '=-' * 5)
+    while quantidade != 0:
+        print(f'{cont}° senha -> ', end='')
+        cont += 1
+        for c in range(0, tamanho):
+            print(f'\033[34m{caractere[randint(0, 72)]}', end='')
+        print('\033[m')
+        quantidade -= 1
